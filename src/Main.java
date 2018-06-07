57d41a23f0c6693c13a66c6ca56da43083ba7735
diwakar1 <diwakar1@umbc.edu>
2018-06-07 16:45:37 -0400

    zork 6/7 4:45

--- src/Main.java
+++ src/Main.java
@@ -1,100 +1,192 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.Random;
 import java.util.Scanner;
 
+import static java.lang.System.out;
+
 public class Main {
 
-    static Boolean isPlaying = true;
+    private static Boolean isPlaying = true;
     static int playerDirection = 0;
-    static int currentRoom;
+    static int currentRoom, roomCounter = 1;
+    static boolean[] roomCount = {false, false, false, false, false, false, false, false};
+    static Boolean skellys = false;
+    static Random rand = new Random(3);
 
     public static void main(String[] args) {
         Scanner player = new Scanner(System.in);
-        String[] roomList = {"Foyer", "Front Room", "Library", "Kitchen", "Dining Room", "Vault","Parlor","Secret Room"};
-        Random rand = new Random();
-        int value = rand.nextInt(9);
-        System.out.println("Zork Introduction HERE");
-
+        int finalRoomCount = 0;
+        out.println("Zork Introduction HERE");
         //while is playing
         while (isPlaying){
-            chooseRoom(currentRoom);
+
+            if (currentRoom > 7){
+                out.println("Thank you for playing!");
+                isPlaying = false;
+            }
+            try {
+                currentRoom = chooseRoom(currentRoom);
+            }
+            catch (Exception e){
+                out.println("Thank you for playing!");
+                isPlaying = false;
+            }
+        }
+        finalRoomCount = roomTotal(roomCount);
+        System.out.print("You entered " + finalRoomCount + " rooms.");
+
+    }
+
+    public static int chooseRoom(int currentRoom) {
+        switch (currentRoom) {
+            case 0:
+                currentRoom = foyerRoom();
+                return currentRoom;
+            case 1:
+                currentRoom = frontRoom();
+                return currentRoom;
+            case 2:
+                currentRoom = libraryRoom();
+                return currentRoom;
+            case 3:
+                currentRoom = kitchenRoom();
+                return currentRoom;
+            case 4:
+                currentRoom = diningRoom();
+                return currentRoom;
+            case 5:
+                currentRoom = vaultRoom();
+                return currentRoom;
+            case 6:
+                currentRoom = parlorRoom();
+                return currentRoom;
+            case 7:
+                currentRoom = secretRoom();
+                return currentRoom;
+            default:
+                return Integer.parseInt(null);
         }
     }
 
-    public static void chooseRoom(int currentRoom){
-        if (currentRoom == 0){
-            foyerRoom();
-            System.out.println(currentRoom + " " + playerDirection);
-            if (playerDirection == 1){
-                currentRoom = 2;
-                System.out.println(currentRoom + " " + playerDirection);
-            }
-        } else if (currentRoom == 1){
-            frontRoom();
-        } else if (currentRoom == 2){
-            libraryRoom();
-        } else if (currentRoom == 3){
-            kitchenRoom();
-        } else if (currentRoom == 4){
-            diningRoom();
-        } else if (currentRoom == 5){
-            vaultRoom();
-        } else if (currentRoom == 6){
-            parlorRoom();
-        } else if (currentRoom == 7){
-            secretRoom();
-        } 
-    }
-    
     public static int foyerRoom() {
         Scanner player = new Scanner(System.in);
-        System.out.print("You are in the Foyer Room.\n"+ "You see a dead scorpion.\nGross..\n" + "{You can enter 1 to exit to the north or press Q to quit.}\n");
+        out.print("You are in the Foyer Room.\n"+ "You see a dead scorpion.\nGross..\n" + "{You can enter 1 to exit to the north or press Q to Quit}\n");
         int playerDirection = player.nextInt();
+        roomCount[0] = true;
+        if (playerDirection != 1){
+            playerDirection = 0;
+        }
         return playerDirection;
     }
-    
-    public static void frontRoom() {
+
+    public static int frontRoom() {
         Scanner player = new Scanner(System.in);
-        System.out.println("Ok, now you are in the Front Room.\n" + "You can play Piano and enjoy here.\n" +
-                "{You can enter 1 to exit to the south, 2 to exit west, 3 to exit to east  or press Q to quit.}");
+        out.println("Ok, now you are in the Front Room.\n" + "You can play Piano and enjoy here.\n" +
+                "{You can enter 1 to exit to the South, 2 to exit West, 3 to exit to East, or press Q to Quit}");
         int playerDirection = player.nextInt();
+        roomCount[1] = true;
+        if (playerDirection == 1){
+            playerDirection = 0;
+        } else if (playerDirection != 2 && playerDirection != 3){
+            playerDirection = 1;
+        }
+        return playerDirection;
     }
 
-    public static void libraryRoom() {
+    public static int libraryRoom() {
         Scanner player = new Scanner(System.in);
-        System.out.println("Ok, now you are in the Library.\n" + "There are lots of spider here, Be Careful!\n" +
-                "{You can enter 1 to exit to the east, 2 to exit to the north or press Q to quit}");
+        out.println("Ok, now you are in the Library.\n" + "There are lots of spider here, Be Careful!\n" +
+                "{You can enter 1 to exit to the East, 2 to exit to the north or press Q to Quit}");
         int playerDirection = player.nextInt();
+        roomCount[2] = true;
+        if (playerDirection == 2){
+            playerDirection = 4;
+        } else if (playerDirection != 1){
+            playerDirection = 2;
+        }
+        return playerDirection;
     }
 
-    public static void kitchenRoom() {
+    public static int kitchenRoom() {
         Scanner player = new Scanner(System.in);
-        System.out.println("Ok, now you are in the Kitchen.\n"+ "There are bats in this room!\n" +
-                "{You can enter 1 to exit to the west, 2 to exit to the north or press Q to quit}");
+        out.println("Ok, now you are in the Kitchen.\n"+ "There are bats in this room!\n" +
+                "{You can enter 1 to exit to the West, 2 to exit to the north or press Q to Quit}");
         int playerDirection = player.nextInt();
+        roomCount[3] = true;
+        if (playerDirection == 2){
+            playerDirection = 6;
+        } else if(playerDirection != 1){
+            playerDirection = 3;
+        }
+        return playerDirection;
     }
 
-    public static void diningRoom() {
+    public static int diningRoom() {
         Scanner player = new Scanner(System.in);
-        System.out.println("Ok, now you are in the Dining Room.\n" + "You can find dust and an empty box in this room.\n" +
-                "{You can enter 1 to exit south or Q to Quit.}");
+        out.println("Ok, now you are in the Dining Room.\n" + "You can find dust and an empty box in this room.\n" +
+                "{You can enter 1 to exit South or Q to Quit}");
         int playerDirection = player.nextInt();
+        roomCount[4] = true;
+        if (playerDirection == 1){
+            playerDirection = 2;
+        } else if (playerDirection != 1){
+            playerDirection = 4;
+        }
+        return playerDirection;
     }
 
-    public static void vaultRoom() {
+    public static int vaultRoom() {
         Scanner player = new Scanner(System.in);
-        System.out.println("watch out , There are 3 walking skeletons\n You can enter 1 to exit East or Q to Quit");
+        int randomChance = rand.nextInt(4);
+        System.out.println(randomChance);
+        if (randomChance > 2){
+            out.println("Watch out, there are 3 walking skeletons!\n{You can enter 1 to exit East, or Q to Quit}");
+            skellys = true;
+        } else {
+            out.println("Welcome to the vault! No skeletons today :)\n{You can enter 1 to exit East, or Q to Quit}");
+        }
         int playerDirection = player.nextInt();
+        roomCount[5] = true;
+        if (playerDirection == 1){
+            playerDirection = 6;
+            //deal with skellys
+        } else if(playerDirection != 1){
+            currentRoom = 5;
+        }
+        return playerDirection;
     }
 
-    public static void parlorRoom() {
+    public static int parlorRoom() {
         Scanner player = new Scanner(System.in);
-        System.out.println("You are in the parlor, This is the place for treasure chest.\n  enter 1 to exit west , 2 to exit South or Q to Quit");
+        out.println("You are in the parlor, this is the place for treasure chest.\n{Enter 1 to exit West, 2 to exit South, or Q to Quit}");
         int playerDirection = player.nextInt();
+        roomCount[6] = true;
+        if (playerDirection == 1){
+            playerDirection = 5;
+        } else if (playerDirection == 2){
+            playerDirection = 3;
+        } else if(playerDirection != 1 && playerDirection != 2){
+            playerDirection = 6;
+        }
+        return playerDirection;
     }
 
-    private static void secretRoom() {
+    private static int secretRoom() {
         Scanner player = new Scanner(System.in);
-        System.out.println("YAY, You are in the Secret room, All of the Gold in here is yours" );
+        out.println("YAY, You are in the Secret room, All of the Gold in here is yours" );
         int playerDirection = player.nextInt();
-    }
+        roomCount[7] = true;
+        isPlaying = false;
+        return playerDirection;
+   }
+
+   private static int roomTotal(boolean[] roomCount){
+       for (boolean aRoomCount : roomCount) {
+           if (aRoomCount) {
+               roomCounter++;
+           }
+       }
+        return roomCounter;
+   }
 }
